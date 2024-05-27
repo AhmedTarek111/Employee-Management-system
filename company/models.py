b47cc535ae9fbacc858debfb0960b61962d49a6e
AhmedTarek111 <ahmedtarekalsaudi@gmail.com>
2024-05-27 23:22:16 +0300

    design the company and department models

--- company/models.py
+++ company/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+class Company(models.Model):
+    name = models.CharField(verbose_name='Company Name',max_length=100)
+    number_of_departments = models.IntegerField(verbose_name='Number of departments')
+    number_of_employees = models.IntegerField(verbose_name='Number of Employees')
+    
+    
+class Department(models.Model):
+    company = models.ForeignKey(Company,on_delete=models.CASCADE,related_name='department_company')
+    name = models.CharField(verbose_name='Department Name',max_length=100,)
+    number_of_employees = models.IntegerField(verbose_name='number_of_employees')
