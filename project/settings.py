2d9ffefd8c63314c9d43a69b5498f424306f4a2b
AhmedTarek111 <ahmedtarekalsaudi@gmail.com>
2024-05-28 02:43:12 +0300

    design user model add

--- project/settings.py
+++ project/settings.py
@@ -127,4 +127,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
 PHONENUMBER_DEFAULT_REGION = 'EG'
+
+AUTH_USER_MODEL = 'accounts.CustomUser'
