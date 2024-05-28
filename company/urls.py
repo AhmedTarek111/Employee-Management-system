501c371bbf1d78bde6070c4b128cae1a99444d5b
AhmedTarek111 <ahmedtarekalsaudi@gmail.com>
2024-05-28 23:45:54 +0300

    solve swagger http delete endpoints

--- company/urls.py
+++ company/urls.py
@@ -4,13 +4,13 @@ urlpatterns = [
     # company endpoints
     path('company/list/',CompanyListAPI.as_view()),
     path('company/create/',CompanyCreateAPI.as_view()),
-    path('company/retrieve-destroy/<int:pk>/',CompanyRetrieveDestroyAPI.as_view),
+    path('company/retrieve-destroy/<int:pk>/',CompanyRetrieveDestroyAPI.as_view()),
     path('company/update/<int:pk>',CompanyUpdateAPI.as_view()),
 
     # department end points 
     path('department/list/',DepartmentListAPI.as_view()),
     path('department/create/',DepartmentCreateAPI.as_view()),
-    path('department/retrieve-destroy/<int:pk>/',DepartmentRetrieveDestroyAPI.as_view),
+    path('department/retrieve-destroy/<int:pk>/',DepartmentRetrieveDestroyAPI.as_view()),
     path('department/update/<int:pk>/',DepartmentUpdateAPI.as_view()),
 
 ]
