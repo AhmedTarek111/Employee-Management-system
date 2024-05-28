2a14e983d7f77a85719362c01459eb0d1cc00e5f
AhmedTarek111 <ahmedtarekalsaudi@gmail.com>
2024-05-29 02:07:18 +0300

    add the missing validation to check the hiredate on the past date not future date

--- employee/serializers.py
+++ employee/serializers.py
@@ -9,15 +9,18 @@ class EmployeeSerializer(serializers.ModelSerializer):
 
     def validate_email(self, value):
         if "@" not in value:
-            raise serializers.ValidationError("This is not a valid email address.")
+            raise serializers.ValidationError("this is not a valid email address")
         return value
 
     def validate_mobile_number(self, value):
         if len(str(value)) < 10:
-            raise serializers.ValidationError("Mobile number must be at least 10 digits long.")
+            raise serializers.ValidationError("mobile number must be at least 10 digits long")
         return value
 
-   
+    def validate(self, data):
+        if data.get('hired_on') and data['hired_on'] > date.today():
+            raise serializers.ValidationError("hired date cannot be in the future")
+        return data
    
     class Meta:
         model = Employee
