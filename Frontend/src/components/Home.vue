f9bdba774d61b31888c36081e6f35d0866684ead
AhmedTarek111 <ahmedtarekalsaudi@gmail.com>
2024-06-01 02:44:16 +0300

    add home page and home button in navbar

--- Frontend/src/components/Home.vue
+++ Frontend/src/components/Home.vue
@@ -1,5 +1,30 @@
 <template>
     <div class="container" >
+        <h2 class="text-center my-5 ">Welcome</h2>
+        <div class="d-flex justify-content-center">
+            <div class="shadow-lg card text-center  bg-primary-subtle me-4" style="width: 18rem; ">
+                <div class="card-body p-4">
+                  <h5 class="card-title">Company Management</h5>
+                  <router-link to="/company/list" class="btn btn-primary mt-3">Go somewhere</router-link>
+                </div>
+              </div>
+            
+            <div class="shadow-lg card text-center  bg-success-subtle me-4 " style="width: 18rem; ">
+                <div class="card-body p-4">
+                  <h5 class="card-title">Department Management</h5>
+                  <router-link to="/department/list/" class="btn btn-success mt-3">Go somewhere</router-link>
+                </div>
+              </div>
+            
+            <div class="shadow-lg card text-center  bg-info-subtle me-4" style="width: 18rem; ">
+                <div class="card-body p-4">
+                  <h5 class="card-title">Employee Management</h5>
+                  <router-link to="/employee/list/" class="btn btn-info mt-3">Go somewhere</router-link>
+                </div>
+              </div>
+            
+              
+        </div>
 
     </div>
 </template>
--- Frontend/src/components/navbar.vue
+++ Frontend/src/components/navbar.vue
@@ -1,6 +1,7 @@
 <template>
     <div class="mb-5">
-        <nav class="navbar bg-dark border-bottom border-body" data-bs-theme="dark">
+        <nav class="navbar bg-dark border-bottom border-body d-flex justify-content-between" data-bs-theme="dark">
+          <router-link to="/" class="btn btn-light ms-5">home </router-link>
           <div class="container-md d-flex justify-content-center">  <router-link class="navbar-brand" to="/">Employee Management System </router-link>
           </div>
         </nav>
